e65b937439596c29212ae7212b7b17ebe24f7556
Thomas Gertenbach <thomas.gertenbach99@gmail.com>
2023-04-14 13:48:06 +0200

    mouse follow

--- src/components/three/MouseFollowMaterial.js
+++ src/components/three/MouseFollowMaterial.js
@@ -97,7 +97,7 @@ const MouseFollowMaterial = shaderMaterial(
   void main()
   {
       vec2 displacedUv = vUv + cnoise(vec3(vUv * 5.0, time * 0.1));
-      float strength = step(0.95, sin(cnoise(vec3(vUv * 3.0, time * 0.1)) * 10.0));  
+      float strength = step(0.999, sin(cnoise(vec3(vUv * 3.0, time * 0.1)) * 10.0));  
       vec3 color = vec3(244.0 / 255.0, 243.0 / 255.0, 232.0 / 255.0);
       float outerGlow = distance(vUv, vec2(0.5)) * 5.0 - 1.4;
       float alpha = 1.0 - smoothstep(0.4, 1.0, outerGlow);
